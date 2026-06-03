63dd26efe13cdd2c35c053fe66d760e6d97f3419
Evil-death09 <yashifytech@gmail.com>
2026-06-03 23:53:26 +0530

    Add string manipulation and type casting examples

---
+++ string_functions.py
@@ -0,0 +1,6 @@
+str = "I am studying python with YouTube "
+print(str.endswith("app"))
+print(str.capitalize()) #capitalizes 1st char
+print(str.replace("o","a")) #replaces with new string
+print(str.find("o")) #returns 1st index of 1st occurrence
+print(str.count("t")) #counts occurrence of substr
---
+++ type_casting.py
@@ -0,0 +1,16 @@
+a="2"
+b=9
+c=int(a)
+print(c+b)
+print(type(a))
+print(type(c))
+
+d=3.14
+e=str(d)
+print(type(d))
+print(type(e))
+name=input("Enter Your name: ")
+print("Hello",name)
+
+val = int(input("enter a num: "))
+print(type(val))
