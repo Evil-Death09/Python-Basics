85521952d41f77ff2423234be839d9dc26faf693
Evil-death09 <yashifytech@gmail.com>
2026-06-04 22:00:30 +0530

    Add list manipulation methods demonstration

---
+++ list_methods.py
@@ -0,0 +1,31 @@
+list =[5,3,8,1,4]
+
+list.append(7) # it will add the element 7 at the end of the list
+print(list)
+
+'''let we have'''
+list1 = ["banana","apple","grapes","orange"]
+
+list.sort() # it will sort the list in ascending order
+print(list)
+list1.sort() # it will sort the list in ascending order
+print(list1)
+
+list.sort(reverse=True) # it will sort the list in descending order
+print(list)
+list1.sort(reverse=True) # it will sort the list in descending order
+print(list1)                
+
+list.reverse() # it will reverse the order of the list
+print(list)
+
+list.insert(2,10) # it will insert the element 10 at index 2
+print(list)
+
+list.remove(3) # it will remove the element 3 from the list
+print(list)
+
+list.pop(2) # it will remove the element at index 2 from the list and return it 
+print(list)
+
+
