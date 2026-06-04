1467114d9c17c4d0241fd8ee0a9b53b88246b74b
Evil-death09 <yashifytech@gmail.com>
2026-06-04 22:11:22 +0530

    tuples in python

---
+++ tuples.py
@@ -0,0 +1,11 @@
+'''A tuple is a built-in data type in Python that is used to store a collection of items. 
+Tuples are ordered, immutable, and allow duplicate values .'''
+'''Tuples are defined by enclosing the items in parentheses () and separating them with commas.'''
+
+tup = (5,3,8,1,4)
+print(tup[0])
+# tup[0] = 10 # we cannot change the value of tuple in python because it is immutable
+
+print(tup[0:3]) # it will print the elements from index 0 to index 2
+print(tup[:3]) # it will print the elements from index 0 to index 2
+print(tup[2:4]) # it will print the elements from index 2 to
