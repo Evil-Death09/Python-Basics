997dec0605c5b09c1b48c146a61d5a7f128d5faf
Evil-death09 <yashifytech@gmail.com>
2026-06-04 22:14:56 +0530

    methods in tuple

---
+++ tuple_methods.py
@@ -0,0 +1,8 @@
+tup = (5, 3, 8, 1, 4)
+
+tup.index(8) # it will return the index of the first occurrence of the element 8 in the tuple
+print(tup.index(8))
+
+tup.count(3) # it will return the number of times the element 3 appears in the tuple
+print(tup.count(3))
+
