93d066dbb61a3de73981548f24964b214c4d7c10
Evil-death09 <yashifytech@gmail.com>
2026-06-05 00:31:25 +0530

    Add nested dictionary example for student data and subject scores

---
+++ nested_dict.py
@@ -0,0 +1,12 @@
+student = {
+    "name": "yash",
+    "subjects" : {
+        "maths" : 96,
+        "phys" : 89,
+        "chem" : 92
+    }
+}
+
+print(student)
+print(student["subjects"])
+print(student["subjects"]["phys"])
