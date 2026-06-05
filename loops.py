7eda098548c035b55b1ed136016bbbb2c044a3b6
Evil-death09 <yashifytech@gmail.com>
2026-06-05 20:13:11 +0530

    While loop

---
+++ loops.py
@@ -0,0 +1,13 @@
+'''Loops are used to repeat a block of code multiple times.'''
+
+i = 0
+while i<=10000 :
+    print("Hello Yash",i)
+    i += 1
+
+#numbers from 100 to 0
+i = 100
+while i>=0 :
+    print(i)
+    i -= 1
+    
