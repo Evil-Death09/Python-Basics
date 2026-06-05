831579047779e827cf4c506951893a908571ea30
Evil-death09 <yashifytech@gmail.com>
2026-06-05 21:03:00 +0530

    Add program to print squares of numbers and search in a tuple

---
+++ loop_problems/problem2.py
@@ -0,0 +1,22 @@
+'''Print the elements of the following list using loop'''
+'''[1,4,9,16,25,36,49,64,81,100]'''
+
+i =1
+list = []
+while i<=10:
+    list.append(i**2)
+    i += 1
+print(list)
+
+'''Search for a number x in this tuple using loop'''
+tup = (1,4,9,16,25,36,49,64,81,100)
+x = int(input("Enter a number to search in the tuple: "))
+i = 0
+while i<len(tup):
+    if tup[i]==x:
+        print(x,"is found in the tuple at index",i)
+        break
+    i += 1 
+else:
+     print(x,"is not found in the tuple")
+
