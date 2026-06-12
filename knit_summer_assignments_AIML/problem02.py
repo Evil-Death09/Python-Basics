2b87a3e83e994ab0835d98709622b5bfcb93f33d
Evil-death09 <yashifytech@gmail.com>
2026-06-12 12:53:11 +0530

    Add birthday countdown program to calculate days left until next birthday

--- knit_summer_assignments_AIML/problem02.py
+++ knit_summer_assignments_AIML/problem02.py
@@ -1,3 +1,29 @@
 '''WAP that calculates your number of days left until the user nesxt birthday based on the current date. '''
 
-birth_date = int(input("Enter your birth date: " ))
+from datetime import date
+
+current_date = date.today()
+print("Today is: ",current_date)
+
+birth_day = int(input("Enter your birth day: "))
+birth_month = int(input("Enter your birth month: "))
+
+next_birthday = date(current_date.year,birth_month,birth_day)
+print("Your next birthday is: ",next_birthday)
+
+if next_birthday == current_date:
+    print("Happy Birthday!!!")
+
+elif next_birthday < current_date:
+    next_birthday = date(current_date.year +1,birth_month,birth_day)
+    days_left = (next_birthday - current_date).days
+    print(f"{days_left} days left for your bithday...")
+    print("Happy Birthday in advance..")
+
+else:
+    days_left = (next_birthday - current_date).days
+    print(f"{days_left} days left for your bithday")
+    print("Happy Birthday in advance..")
+
+
+
